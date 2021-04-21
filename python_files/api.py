c372522f35b913f632df7fff27280be3503688dd
chanok <Chanok_boon@hotmail.com>
2021-04-21 16:52:09 +0700

    Update api.py

--- python_files/api.py
+++ python_files/api.py
@@ -2,13 +2,10 @@ import json
 import urllib
 import requests
 
-url = 'https://parseapi.back4app.com/classes/Coronaviruscases_Covid19Case?count=1&limit=195&order=cases'
+url = 'https://parseapi.back4app.com/classes/Covid19Case?count=1&limit=10'
 headers = {
-    'X-Parse-Application-Id': '8bnYdl1uCG7ygDkHCknb1FBZABAyYtgs5dIwRVir', # This is your app's application id
-    'X-Parse-REST-API-Key': 'LOFKb4T5SpYX9WlECGL20kzsNkCI8OkKhnlgPQwx' # This is your app's REST API key
+    'X-Parse-Application-Id': 'zoZ3zW1YABEWJMPInMwruD5XHgqT4QluDAAVx0Zz', # This is the fake app's application id
+    'X-Parse-Master-Key': 'gIo7p0nTyt72aROJqf0ronfzxGKw8Unjw0Zk6qFm' # This is the fake app's readonly master key
 }
 data = json.loads(requests.get(url, headers=headers).content.decode('utf-8')) # Here you have the data that you need
 print(json.dumps(data, indent=2))
-
-#for editing query data
-#https://www.back4app.com/database/back4app/coronavirus-covid-19-api/get-started/python/rest-api/requests?appEntityId=ecd8bdeb-3394-4bee-9e48-67637bb85fc8
