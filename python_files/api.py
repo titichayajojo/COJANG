cabca29c0edc52258f4c423380e96aefdd24a656
teelekxx <62011299@kmitl.ac.th>
2021-04-21 19:49:41 +0700

    api to csv

--- python_files/api.py
+++ python_files/api.py
@@ -1,11 +1,21 @@
 import json
 import urllib
 import requests
+import pandas
+import pyrebase
+import csv
 
 url = 'https://parseapi.back4app.com/classes/Covid19Case?count=1'
 headers = {
     'X-Parse-Application-Id': 'zoZ3zW1YABEWJMPInMwruD5XHgqT4QluDAAVx0Zz', # This is the fake app's application id
     'X-Parse-Master-Key': 'gIo7p0nTyt72aROJqf0ronfzxGKw8Unjw0Zk6qFm' # This is the fake app's readonly master key
 }
-data = json.loads(requests.get(url, headers=headers).content.decode('utf-8')) # Here you have the data that you need
-print(json.dumps(data, indent=2))
+csvfile = open('codata.csv','w', newline='')
+obj=csv.writer(csvfile)
+obj.writerow(('countryName', 'cases', 'deaths', 'recovered'))
+path = requests.get(url, headers=headers).content.decode('utf-8')
+data = json.loads(path) # Here you have the data that you need
+for k in data['results']:
+    obj.writerow((k['countryName'], k['cases'], k['deaths'], k['recovered']))
+
+csvfile.close()
