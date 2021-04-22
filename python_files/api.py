d2ba7ed0820196241007c2514a71144969b6c036
teelekxx <62011299@kmitl.ac.th>
2021-04-22 14:33:52 +0700

    usa data

--- python_files/api.py
+++ python_files/api.py
@@ -5,17 +5,25 @@ import pandas
 import pyrebase
 import csv
 
-url = 'https://parseapi.back4app.com/classes/Covid19Case?count=1'
+url = 'https://api.apify.com/v2/datasets/FIbyK6uHUntt2kNy3/items?format=json&clean=1'
 headers = {
     'X-Parse-Application-Id': 'zoZ3zW1YABEWJMPInMwruD5XHgqT4QluDAAVx0Zz', # This is the fake app's application id
     'X-Parse-Master-Key': 'gIo7p0nTyt72aROJqf0ronfzxGKw8Unjw0Zk6qFm' # This is the fake app's readonly master key
 }
 csvfile = open('codata.csv','w', newline='')
-obj=csv.writer(csvfile)
-obj.writerow(('countryName', 'cases', 'deaths', 'recovered'))
+obj = csv.writer(csvfile)
+obj.writerow(('state', 'cases', 'date'))
 path = requests.get(url, headers=headers).content.decode('utf-8')
 data = json.loads(path) # Here you have the data that you need
-for k in data['results']:
-    obj.writerow((k['countryName'], k['cases'], k['deaths'], k['recovered']))
+state = None
+cases = None
+date = None
+for i in data:
+    for j in range(len(i['casesByState'])):
+        print(j)
+        state = i['casesByState'][j]['name']
+        cases = i['casesByState'][j]['casesReported']
+        date = i['lastUpdatedAtSource']
+        obj.writerow((state, cases, date))
 
 csvfile.close()
