b7e1d74dd022f7150f91ac55d88d8e265d800f33
chanok <Chanok_boon@hotmail.com>
2021-04-22 21:35:14 +0700

    edit

--- python_files/main.py
+++ python_files/main.py
@@ -1,114 +1,106 @@
-import json
-import pandas as pd
-import csv
+# import json
+# import pandas as pd
+# import csv
 
-import dash  # (version 1.12.0) pip install dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
+# import dash  # (version 1.12.0) pip install dash
+# import dash_core_components as dcc
+# import dash_html_components as html
+# from dash.dependencies import Input, Output
 
-# df = pd.read_csv("codata.csv")
-# df2 = df[df['date' ] == "2020-03-13"]
-# print(df2)
+# # df = pd.read_csv("codata.csv")
+# # df2 = df[df['date' ] == "2020-03-13"]
+# # print(df2)
 
-import dash_core_components as dcc
+# import dash_core_components as dcc
 
 
-import pandas as pd
-import plotly.express as px  # (version 4.7.0)
-import plotly.graph_objects as go
+# import pandas as pd
+# import plotly.express as px  # (version 4.7.0)
+# import plotly.graph_objects as go
 
-import dash  # (version 1.12.0) pip install dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
+# import dash  # (version 1.12.0) pip install dash
+# import dash_core_components as dcc
+# import dash_html_components as html
+# from dash.dependencies import Input, Output
 
-app = dash.Dash(__name__)
+# app = dash.Dash(__name__)
 
-# ---------- Import and clean data (importing csv into pandas)
-# df = pd.read_csv("intro_bees.csv")
-df = pd.read_csv("https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plotly/master/Other/Dash_Introduction/intro_bees.csv")
+# # ---------- Import and clean data (importing csv into pandas)
+# # df = pd.read_csv("intro_bees.csv")
+# df = pd.read_csv("https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plotly/master/Other/Dash_Introduction/intro_bees.csv")
 
-df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of Colonies Impacted']].mean()
-df.reset_index(inplace=True)
-print(df[:5])
+# df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of Colonies Impacted']].mean()
+# df.reset_index(inplace=True)
+# print(df[:5])
 
-# ------------------------------------------------------------------------------
-# App layout
-app.layout = html.Div([
+# # ------------------------------------------------------------------------------
+# # App layout
+# app.layout = html.Div([
 
-    html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
+#     html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
 
-    dcc.Dropdown(id="slct_year",
-                 options=[
-                     {"label": "2015", "value": 2015},
-                     {"label": "2016", "value": 2016},
-                     {"label": "2017", "value": 2017},
-                     {"label": "2018", "value": 2018}],
-                 multi=False,
-                 value=2015,
-                 style={'width': "40%"}
-                 ),
+#     dcc.Dropdown(id="slct_year",
+#                  options=[
+#                      {"label": "2015", "value": 2015},
+#                      {"label": "2016", "value": 2016},
+#                      {"label": "2017", "value": 2017},
+#                      {"label": "2018", "value": 2018}],
+#                  multi=False,
+#                  value=2015,
+#                  style={'width': "40%"}
+#                  ),
 
-    html.Div(id='output_container', children=[]),
-    html.Br(),
+#     html.Div(id='output_container', children=[]),
+#     html.Br(),
 
-    dcc.Graph(id='my_bee_map', figure={}),
+#     dcc.Graph(id='my_bee_map', figure={}),
     
-dcc.Slider(
-    min=0,
-    max=10,
-    step=None,
-    marks={
-        0: '0 °F',
-        3: '3 °F',
-        5: '5 °F',
-        7.65: '7.65 °F',
-        10: '10 °F'
-    },
-    value=5
-)  
+# dcc.Slider(
+#     step=None,
+#     marks={},
+    
+# )  
 
 
-])
+# ])
 
 
-# ------------------------------------------------------------------------------
-# Connect the Plotly graphs with Dash Components
-@app.callback(
-    [Output(component_id='output_container', component_property='children'),
-     Output(component_id='my_bee_map', component_property='figure')],
-    [Input(component_id='slct_year', component_property='value')]
-)
-def update_graph(option_slctd):
-    print(option_slctd)
-    print(type(option_slctd))
+# # ------------------------------------------------------------------------------
+# # Connect the Plotly graphs with Dash Components
+# @app.callback(
+#     [Output(component_id='output_container', component_property='children'),
+#      Output(component_id='my_bee_map', component_property='figure')],
+#     [Input(component_id='slct_year', component_property='value')]
+# )
+# def update_graph(option_slctd):
+#     print(option_slctd)
+#     print(type(option_slctd))
 
-    container = "The year chosen by user was: {}".format(option_slctd)
+#     container = "The year chosen by user was: {}".format(option_slctd)
 
-    dff = df.copy()
-    dff = dff[dff["Year"] == option_slctd]
-    dff = dff[dff["Affected by"] == "Varroa_mites"]
+#     dff = df.copy()
+#     dff = dff[dff["Year"] == option_slctd]
+#     dff = dff[dff["Affected by"] == "Varroa_mites"]
 
-    # Plotly Express
-    fig = px.choropleth(
-        data_frame=dff,
-        locationmode='USA-states',
-        locations='state_code',
-        scope="usa",
-        color='Pct of Colonies Impacted',
-        hover_data=['State', 'Pct of Colonies Impacted'],
-        color_continuous_scale=px.colors.sequential.YlOrRd,
-        labels={'Pct of Colonies Impacted': '% of Bee Colonies'},
-        template='plotly_dark'
-    )
+#     # Plotly Express
+#     fig = px.choropleth(
+#         data_frame=dff,
+#         locationmode='USA-states',
+#         locations='state_code',
+#         scope="usa",
+#         color='Pct of Colonies Impacted',
+#         hover_data=['State', 'Pct of Colonies Impacted'],
+#         color_continuous_scale=px.colors.sequential.YlOrRd,
+#         labels={'Pct of Colonies Impacted': '% of Bee Colonies'},
+#         template='plotly_dark'
+#     )
     
 
     
 
-    return container, fig
+#     return container, fig
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
 
     
