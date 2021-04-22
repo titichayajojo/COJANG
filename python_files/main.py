838ab6b9e7d69dc9630473135b674bbeca3af064
chanok <Chanok_boon@hotmail.com>
2021-04-22 17:00:41 +0700

    Update main.py

--- python_files/main.py
+++ python_files/main.py
@@ -1,25 +1,17 @@
 import json
 import pandas as pd
 
-usa_states = json.load(open("others/united_state.json",'r'))
-print(usa_states['features'][1])
+# usa_states = json.load(open("others/united_state.json",'r'))
+# print(usa_states['features'][1])
+# df = pd.read_csv("codata.csv")
+# df['Cases'] = df['cases']
+# print(df['Cases'])
+# state_id_map = {}
+# for feature in usa_states['features']:
+#     feature['uni_id'] = feature['properties']['STATE']
+#     state_id_map[feature['properties']['NAME']] = feature['uni_id']
+# print(state_id_map)
+# print(df['uni_id'])
+
 df = pd.read_csv("codata.csv")
-df['Cases'] = df['cases']
-print(df['Cases'])
-state_id_map = {}
-for feature in usa_states['features']:
-    feature['uni_id'] = feature['properties']['STATE']
-    state_id_map[feature['properties']['NAME']] = feature['uni_id']
-print(state_id_map)
-<<<<<<< Updated upstream
-df['uni_id'] = df['state'].apply(lambda x: state_id_map[x] if state_id_map[x] else )
-=======
-df['uni_id'] = df['state'].apply(lambda x: state_id_map[x] if state_id_map[x] else "No value")
->>>>>>> Stashed changes
 
-
-
-
-
-
-print(df['uni_id'])
